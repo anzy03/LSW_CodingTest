536a1637bf47aa4cc2e3c97b0d0eb6f1d80bca4e
Arnav <arnav.sanghavi@gmail.com>
2021-08-06 15:31:51 -0700

    [Done] basic functionality

--- Assets/Scripts/World/Shop.cs
+++ Assets/Scripts/World/Shop.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<PlayerController>() == true)
         {
+            Debug.Log("PlayerDetected");
             //TODO start buying prompt
         }
     }
